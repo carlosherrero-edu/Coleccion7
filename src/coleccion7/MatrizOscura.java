f2b425340c09e07a0add59470329c4154c248342
carlosherrero-edu <carlosherrero.edu@gmail.com>
2021-02-28 17:45:27 +0100

    Solución y clase de prueba para el ejercicio 12

--- src/coleccion7/MatrizOscura.java
+++ src/coleccion7/MatrizOscura.java
@@ -2,9 +2,52 @@ package coleccion7;
 
 public class MatrizOscura {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static final int MINIMO_PIXELES = 36;
 
+	/**
+	 * Método estático que determina si una imagen pasada como matriz de pixeles se
+	 * considera oscura La imagen se considera oscura si una proporción de sus
+	 * pixeles superior o igual al porcentaje indicado tiene un valor de intensidad
+	 * inferior al umbral
+	 * 
+	 * @param imagen     matriz de pixeles que representa una imagan
+	 * @param umbral     valor por debajo del cual un pixel se considera oscuro
+	 * @param porcentaje porcentaje mínimo de pixeles para que la imagen se
+	 *                   considere oscura
+	 * @return true/false
+	 */
+	public static boolean esOscura(int[][] imagen, int umbral, double porcentaje) {
+		// paso 1:  calculamos número de pixeles de la imagen
+
+		int num_pixeles = imagen.length * imagen[0].length;
+
+		// Paso 2: validación de parámetros y condiciones de error
+		if (num_pixeles < MINIMO_PIXELES)
+			throw new IllegalArgumentException("Imagen con una resolución demasiado baja");
+		else if (umbral < 0 || umbral > 255)
+			throw new IllegalArgumentException("El umbral debe estar entre 0 y 255");
+		else if (porcentaje < 0 || porcentaje > 100)
+			throw new IllegalArgumentException("El porcentaje debe estar entre 0 y 100");
+
+	
+
+		// paso 3: recorremos todos los pixeles y contamos los oscuros
+		
+		int oscuros = 0;
+
+		for (int i = 0; i < imagen.length; i++) {
+			for (int j = 0; j < imagen[0].length; j++) {
+				if (imagen[i][j] < umbral)
+					oscuros++;
+			}
+		}
+		/* Paso 4: Devolvermos resultado
+		
+		destacar que hay que hacer una conversión explícita integer--> double de los enteros
+		para que la división no resulte siempre 0 por el truncamiento de decimales
+		*/
+		return (double)oscuros / (double)num_pixeles >= porcentaje * 0.01;
+		
 	}
 
 }
--- test/coleccion7/MatrizOscuraTest.java
+++ test/coleccion7/MatrizOscuraTest.java
@@ -3,6 +3,8 @@ package coleccion7;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 
 public class MatrizOscuraTest {
 
@@ -22,50 +24,57 @@ public class MatrizOscuraTest {
 			{150, 150, 150, 200, 200} };
 
 	@Test
-	public void testEsImagenOscura1() {
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,200,75));
-		assertFalse(MatrizOscura.esImagenOscura(imagen1,200,75.01));
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,150,50));
-		assertFalse(MatrizOscura.esImagenOscura(imagen1,150,50.1));
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,51,33.34));
-		assertFalse(MatrizOscura.esImagenOscura(imagen1,50,33.34));
+	@DisplayName("Probando con distintos umbrales y métodos")
+	public void testEsImagenOscura1() throws Exception {
+		Assertions.assertAll(
+				()->assertTrue(MatrizOscura.esOscura(imagen1,200,75)),
+				()->assertFalse(MatrizOscura.esOscura(imagen1,200,75.01)),
+				()->assertTrue(MatrizOscura.esOscura(imagen1,150,50)),
+				()->assertFalse(MatrizOscura.esOscura(imagen1,150,50.01)),
+				()->assertTrue(MatrizOscura.esOscura(imagen1,51,100/3)),
+				()->assertFalse(MatrizOscura.esOscura(imagen1,50,100/3))
+				);
 		
 	}
 	
-	@Test (expected = RuntimeException.class)
+	@Test (expected = IllegalArgumentException.class)
+	@DisplayName("Generando excepción para umbral >255")
 	public void testEsImagenOscura2_1() {
 		System.out.println("Debe rechazarlo porque el umbral es > 255");
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,256,50));
+		assertTrue(MatrizOscura.esOscura(imagen1,256,50));
 	}
 		
-		@Test (expected = RuntimeException.class)
+		@Test (expected = IllegalArgumentException.class)
+		@DisplayName("Generando excepción para umbral <0")
 		public void testEsImagenOscura2_2() {
 			System.out.println("Debe rechazarlo porque el umbral es <0");
-			assertTrue(MatrizOscura.esImagenOscura(imagen1,-1,50));
+			assertTrue(MatrizOscura.esOscura(imagen1,-1,50));
 		
 		
 	}
 	
-	@Test (expected = RuntimeException.class)
+	@Test (expected = IllegalArgumentException.class)
+	@DisplayName("Generando excepción para porcentaje >100")
 	public void testEsImagenOscura3_1() {
 		System.out.println("Debe rechazarlo porque el porcentaje es > 100");
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,256,100.01));
+		assertTrue(MatrizOscura.esOscura(imagen1,256,100.01));
 		
 		
 	}
 	
-	@Test (expected = RuntimeException.class)
+	@Test (expected = IllegalArgumentException.class)
+	@DisplayName("Generando excepción para porcentaje < 0")
 	public void testEsImagenOscura3_2() {
 		System.out.println("Debe rechazarlo porque el porcentaje es <0");
-		assertTrue(MatrizOscura.esImagenOscura(imagen1,256, -0.0001));
+		assertTrue(MatrizOscura.esOscura(imagen1,256, -0.0001));
 		
 		
 	}
 	
-	@Test (expected = RuntimeException.class)
+	@Test (expected = IllegalArgumentException.class)
 	public void testEsImagenOscura4() {
 		System.out.println("Debe rechazarlo porque el array tiene < 36 pixeles");
-		assertTrue(MatrizOscura.esImagenOscura(imagen2,150,51));
+		assertTrue(MatrizOscura.esOscura(imagen2,150,51));
 		
 		
 	}
